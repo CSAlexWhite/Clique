be7be59790e2935e0dfc2a790f0b072570b9f767
Alex <alexwhite123@gmail.com>
2015-08-01 09:19:02 -0400

    Now binary strings working okay: realized that generating all of them is brute force, not what we want.  Now can produce as many bit strings of length n with k ones as I want.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Vector;
 
 public class Main {
@@ -23,14 +24,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer n = 5;
-        int k = 3;
-
+        Integer n = 12;
+        int k = 5;
         String padding = "%0" + Integer.toString(n) + "d";
 
-        Vector<String> test = getCombinations(5, 3, padding);
-        printCombinations(test);
+        for(int i=0; i<10; i++){
+
+            System.out.println(generateCombination(n, k, padding));
+        }
     }
+
+    private static String generateCombination(int n, int k, String padding){
+
+        StringBuilder combination = new StringBuilder(String.format(padding, 0)); // create a bitstring with n zerosSystem.outl.print
+
+        int position, count = 0;
+        while(count < k || numberChosen(combination.toString()) < k){
+
+            position = randomInt(0, n-1);
+            combination.setCharAt(position, '1');
+            count++;
+        }
+
+        return combination.toString();
+    }
+
+    private static int numberChosen(String input){
+
+        int count = 0;
+        for(int i=0; i<input.length(); i++){
+
+            if(input.charAt(i) == '1') count++;
+        }
+
+        return count;
+    }
+
+    private static int randomInt(int min, int max) {
+
+        Random rand = new Random(System.currentTimeMillis());
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+
 
     /**
      * Returns a vector of all numbers whose bitstring representation are of length n and include exactly k ones.  To be
@@ -39,16 +77,17 @@ public class Main {
      * @param k
      * @return
      */
-    private static Vector<String> getCombinations(int n, int k, String p){
+    private static Vector<String> getAllCombinations(long n, long k, String padding){
 
         Vector<String> combinations = new Vector<String>();
 
-        int x = (int)(Math.pow(2, k) - 1);  // initialize x
-        int count = 1;// x is the first combination
-        int u = 0, v = 0, y = 0;
+        long x = (long)(Math.pow(2, k) - 1); //+ (int)(Math.pow(2, n) - 1);  // initialize x
+        long count = 1;// x is the first combination
+        long u = 0, v = 0, y = 0;
+
         while(count <= combinations(n, k)){
 
-            combinations.add(String.format(p, Integer.parseInt(Integer.toBinaryString(x))));
+            combinations.add(String.format(padding, Long.parseLong(Long.toBinaryString(x))));
 
             u = x & -x;
             v = x + u;
@@ -61,7 +100,7 @@ public class Main {
         return combinations;
     }
 
-    private static void printCombinations(Vector<String> vector){
+    private static void printStringVector(Vector<String> vector){
 
         for(int i=0; i<vector.size(); i++){
 
@@ -69,14 +108,14 @@ public class Main {
         }
     }
 
-    private static int combinations(int n, int k){
+    private static long combinations(long n, long k){
 
         return factorial(n)/(factorial(k) * factorial(n - k));
     }
 
-    private static int factorial(int n) {
+    private static long factorial(long n) {
 
-        int fact = 1;
+        long fact = 1;
         for (int i = 1; i <= n; i++) {
             fact *= i;
         }
